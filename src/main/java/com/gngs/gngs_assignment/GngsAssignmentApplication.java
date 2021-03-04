74a671b6e02942d36e6fcc3a6c8328a0a1c0a76c
chinmichael <chinmichael@naver.com>
2021-03-05 01:00:29 +0900

    spring boot & git setting

---
+++ src/main/java/com/gngs/gngs_assignment/GngsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.gngs.gngs_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GngsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GngsAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gngs/gngs_assignment/GngsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gngs.gngs_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GngsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
